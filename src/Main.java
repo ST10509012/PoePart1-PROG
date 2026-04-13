ac3729274daf588b2b46295a377d873b86946a45
ST10509012 <ST10509012@rcconnect.edu.za>
2026-04-13 23:13:51 +0200

    DONE 6.

--- src/Main.java
+++ src/Main.java
@@ -8,9 +8,9 @@ void main() {
     String fName = scanner.nextLine();
     System.out.println(" Enter your Last Name: ");
     String lName = scanner.nextLine();
-    System.out.println(" Enter your Username" + "(ensure that the password contains at least eight characters, a capital letter, a number, and a spec):");
+    System.out.println(" Enter your Username" + "(ensure that your username contains an underscore and is no more than five characters in length):");
     String username = scanner.nextLine();
-    System.out.println(" Enter your Password" + "(ensure that your username contains an underscore and is no more than five characters in length):");
+    System.out.println(" Enter your Password" + "(ensure that the password contains at least eight characters, a capital letter, a number, and a spec):");
     String password = scanner.nextLine();
     System.out.println(" Enter your Cell Phone Number" + "(ensure that your number starts [+27] ");
     String cell = scanner.nextLine();
